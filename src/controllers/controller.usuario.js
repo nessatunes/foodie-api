df06433dd19c56b8c605858ae21ea1d4b1f794fe
Nessatunes <essatunes@gmail.com>
2024-09-04 20:26:26 -0300

    feat: add favorite restaurant

---
+++ src/controllers/controller.usuario.js
@@ -0,0 +1,50 @@
+import serviceUsuario from "../services/service.usuario.js";
+
+async function Favoritos(req, res) {
+  try {
+    const id_usuario = 1; // pegar do token jwt
+    const favoritos = await serviceUsuario.Favoritos(id_usuario);
+
+    res.status(200).json(favoritos);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+}
+
+async function Login(req, res) {
+  //const email = req.body.email;
+  //const senha = req.body.senha;
+  const { email, senha } = req.body;
+
+  if (email == "teste@teste.com" && senha == "12345") {
+    res.status(200).json({
+      id_usuario: 123,
+      email: "teste@teste.com",
+      nome: "Heber Stein Mazutti",
+      insta: "@devpoint.com.br",
+    });
+  } else {
+    res.status(401).json({ error: "E-mail ou senha inválida" });
+  }
+}
+
+async function Inserir (req, res) {
+  const { nome, email, senha, endereco, complemento, bairro, cidade, uf, cep } =
+    req.body;
+
+  res.status(201).json({
+    id_usuario: 123,
+    nome,
+    email,
+    senha,
+    endereco,
+    complemento,
+    bairro,
+    cidade,
+    uf,
+    cep,
+    insta: "@devpoint.com.br",
+  });
+};
+
+export default { Favoritos, Login, Inserir };
--- src/routes.js
+++ src/routes.js
@@ -3,62 +3,19 @@ import controllerCategoria from "./controllers/controller.categoria.js";
 import controllerBanner from "./controllers/controller.banner.js";
 import controllerEmpresa from "./controllers/controller.empresa.js";
 import controllerPedido from "./controllers/controller.pedido.js";
+import controllerUsuario from "./controllers/controller.usuario.js";
 
 const router = Router();
 
-//GET: http://localhost:3001/usuarios/login?email=teste@teste.com&senha=123
-
-router.post("/usuarios/login", (req, res) => {
-  //const email = req.body.email;
-  //const senha = req.body.senha;
-  const { email, senha } = req.body;
-
-  if (email == "teste@teste.com" && senha == "12345") {
-    res.status(200).json({
-      id_usuario: 123,
-      email: "teste@teste.com",
-      nome: "Heber Stein Mazutti",
-      insta: "@devpoint.com.br",
-    });
-  } else {
-    res.status(401).json({ error: "E-mail ou senha inválida" });
-  }
-});
-
-router.post("/usuarios", (req, res) => {
-  const { nome, email, senha, endereco, complemento, bairro, cidade, uf, cep } =
-    req.body;
-
-  res.status(201).json({
-    id_usuario: 123,
-    nome,
-    email,
-    senha,
-    endereco,
-    complemento,
-    bairro,
-    cidade,
-    uf,
-    cep,
-    insta: "@devpoint.com.br",
-  });
-});
-
-router.get("/restaurantes", (req, res) => {
-  // URI Params: http://localhost:3001/restaurantes/10
-  // Query params: http://localhost:3001/restaurantes?busca=Pizza (somente GET)
-  const busca = req.query.busca;
-
-  res.json([
-    { restaurante: 1, nome: "Burger King" },
-    { restaurante: 2, nome: "Mc Donalds" },
-  ]);
-});
-
 router.get("/categorias", controllerCategoria.Listar);
 router.get("/banners", controllerBanner.Listar);
+
 router.get("/empresas/destaques", controllerEmpresa.Destaques);
 router.get("/pedidos", controllerPedido.Listar);
 router.get("/pedidos/:id_pedido", controllerPedido.ListarId);
+
+router.get("/usuarios/favoritos", controllerUsuario.Favoritos);
+router.post("/usuarios/login", controllerUsuario.Login);
+router.post("/usuarios", controllerUsuario.Inserir);
 
 export default router;
